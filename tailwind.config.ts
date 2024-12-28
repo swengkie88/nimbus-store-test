1c3715de6fcd81cd6e9ebd71a117991662c54249
swengkie88 <angkiprana1987@gmail.com>
2024-12-29 02:23:06 +0700

    Update some configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,9 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 } satisfies Config;
