3944702cd929a8dcebf2e391b905298b93098995
swengkie88 <angkiprana1987@gmail.com>
2024-12-29 20:55:36 +0700

    Add preline ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,15 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'selector',
+  content: [
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/preline/dist/*.js',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 } satisfies Config;
