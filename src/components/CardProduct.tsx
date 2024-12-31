04a22923c220cb60ab255cc1d1f78c296f3e97fd
swengkie88 <angkiprana1987@gmail.com>
2025-01-01 01:16:31 +0700

    Display object rating from response API to CardProduct

--- src/components/CardProduct.tsx
+++ src/components/CardProduct.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { Star, StarHalf } from "@phosphor-icons/react";
 import Image from "next/image"
 import { Product } from "types/product";
 
@@ -8,6 +9,11 @@ interface CardProductProps {
 }
 
 export default function CardProduct({ product }: CardProductProps) {
+    const { rate } = product.rating; // Mengambil nilai rating dari product
+    const fullStars = Math.floor(rate); // Bintang penuh
+    const hasHalfStar = rate % 1 >= 0.5; // Jika ada setengah bintang
+    const emptyStars = 5 - fullStars - (hasHalfStar ? 1 : 0); // Sisanya adalah bintang kosong
+
     return (
         <div className="flex flex-col justify-center items-center gap-4">
             <div className="h-full sm:h-90 flex items-center relative rounded-xl overflow-hidden">
@@ -17,6 +23,18 @@ export default function CardProduct({ product }: CardProductProps) {
             <div className="flex-col justify-center items-center gap-3 flex">
                 <div className="flex-col justify-center text-center items-center gap-1 flex">
                     <h5 className="text-slate-900 text-sm sm:text-lg font-medium font-['Instrument Sans'] leading-7">{product.title}</h5>
+                </div>
+                {/* Visualisasi bintang */}
+                <div className="flex justify-start items-center">
+                    {Array.from({ length: fullStars }).map((_, index) => (
+                        <Star key={`full-${index}`} size={20} weight="fill" className="text-yellow-500" />
+                    ))}
+                    {hasHalfStar && (
+                        <StarHalf key="half" size={20} weight="fill" className="text-yellow-500" />
+                    )}
+                    {Array.from({ length: emptyStars }).map((_, index) => (
+                        <Star key={`empty-${index}`} size={20} weight="regular" className="text-gray-300" />
+                    ))}
                 </div>
                 <div className="justify-start items-center gap-2 inline-flex">
                     <p className="text-slate-900 text-sm sm:text-base font-medium font-['Instrument Sans'] leading-normal">${product.price}</p>
--- src/types/product.ts
+++ src/types/product.ts
@@ -1,9 +1,14 @@
-// types/product.ts
+export interface Rating {
+    rate: number;
+    count: number;
+}
+
 export interface Product {
-    id: number;
+    id: number; // ID produk
     title: string; // Nama produk
     price: number; // Harga produk
     description: string; // Deskripsi produk
     category: string; // Kategori produk
     image: string; // URL gambar produk
+    rating: Rating; // Objek rating produk
 }
